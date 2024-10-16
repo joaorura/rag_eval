626cb454eb25defe2096aba35f82b7d11b6264d9
João Messias Lima Pereira <jmessiaslp856@gmail.com>
2024-10-16 13:49:53 -0300

    up

--- test.py
+++ test.py
@@ -1,10 +1,39 @@
-import json
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# String JSON com aspas escapadas
-json_string = '\"{\\"feedback\": \\"Esta quest\u00e3o\\"}"'
+# Carregar os dados dos arquivos CSV
+test1 = pd.read_csv('result_opennai_llama3_1.csv')
+test2 = pd.read_csv('result_opennai_llama3_2_3b.csv')
 
-# Decodificar a string JSON
-decoded_json = json.loads(json_string)
+# Adicionar uma coluna para identificar a origem dos dados
+test1['Modelos'] = 'Llama 3.1 8b'
+test2['Modelos'] = 'Llama 3.2 3b'
 
-# Imprimir o resultado
-print(decoded_json)
+# Combinar os dois DataFrames
+combined_df = pd.concat([test1, test2])
+
+# Filtrar as métricas desejadas
+metrics = [
+    "answer_relevancy",
+    "answer_correctness",
+    "answer_similarity",
+    "context_precision",
+    "context_recall",
+    "context_utilization",
+    "context_entity_recall",
+]
+filtered_df = combined_df[['Modelos'] + metrics]
+
+print(filtered_df)
+# Derreter o DataFrame para o formato longo
+melted_df = filtered_df.melt(id_vars='Modelos', var_name='Métricas', value_name='Valores')
+
+print(melted_df)
+# Criar o gráfico de barras
+plt.figure(figsize=(18, 14))
+sns.barplot(data=melted_df, x='Métricas', y='Valores', hue='Modelos')
+plt.title('Avaliação RAG default com os modelos Llama 3.1 8b e Llama 3.2 3b')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
